dfd4579b8bce922181d910c378b404b91a6a3705
Paul Boes <paul.boes@porsche.digital>
2023-06-01 22:33:16 +0200

    renaming app into main for Deta requirements

--- app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from main import Data, SolverOutput, solve_func
-
-app = FastAPI()
-
-@app.get("/")
-def run_model(data: Data) -> SolverOutput:
-    return solve_func(data)
